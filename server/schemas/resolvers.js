a540e542248319cb76a30924b9164a1b5ba0f67d
Kayla <kyairlakayla.davis@gmail.com>
2021-11-24 14:37:49 -0500

    finish resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -19,6 +19,48 @@ const resolvers = {
             throw new AuthenticationError('Not logged in');
         }
     },
-}
+    Mutation: {
+        // creates a user and issues them a token
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            // create the token
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        // verifies user exits and issues them a token
+        login: async (parent, {email, password}) => {
+            const user = await User.findOne({email});
+
+            if(!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect Credentials');
+            }
+
+            // create the token
+            const token = signToken(user);
+            return { token, user };
+        },
+        // save a book
+        saveBook: async (parent, { book }, context) => {
+            if (context.user) {
+                const addBook = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: book } },
+                    { new: true }
+                );
+
+                return addBook;
+            }
+
+            throw new AuthenticationError('You need to be logged in!');
+        }
+    }
+};
 
 module.exports = resolvers;
